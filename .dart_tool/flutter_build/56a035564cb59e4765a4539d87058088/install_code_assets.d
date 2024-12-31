c3cb3e0183e2ca379be781989c30add3ed782501
GitDang <minhdangit95@gmail.com>
2024-12-31 19:08:42 +0900

    commit source base

---
+++ .dart_tool/flutter_build/56a035564cb59e4765a4539d87058088/install_code_assets.d
@@ -0,0 +1,1 @@
+ /Users/a/Desktop/MMO/SnakeGame/SnackGame/.dart_tool/flutter_build/56a035564cb59e4765a4539d87058088/native_assets.json: 
---
+++ android/app/src/main/kotlin/com/danglinh/snakebaby/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.danglinh.snakebaby
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
